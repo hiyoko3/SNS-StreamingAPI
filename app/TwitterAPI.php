f22281bade3bdb13c43ca878081c0eb7fedca86c
tatsuya_oyanagi <17nm704r@vc.ibaraki.ac.jp>
2018-04-13 20:27:48 +0900

    Create github repository.

--- app/TwitterAPI.php
+++ app/TwitterAPI.php
@@ -3,66 +3,39 @@ namespace App;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Spatie\TwitterStreamingApi\PublicStream;
+use OauthPhirehose;
+use Phirehose;
 
+// parse XML
 $xml = simplexml_load_file(__DIR__ . '/../API_Key.xml');
 $xmlObj = get_object_vars($xml);
-var_dump($xmlObj['twitter']);
+$twitter = $xmlObj['twitter'];
 
-//PublicStream::create(
-//    $twitter['oauth_token'],
-//    $twitter['oauth_secret'],
-//    $twitter['consumer_key'],
-//    $twitter['cunsumer_secret']
-//)->whenHears('@spatie_be', function(array $tweet) {
-//    echo "We got mentioned by {$tweet['user']['screen_name']} who tweeted {$tweet['text']}";
-//})->checkFilterPredicates(function($stream) {
-//    $trackIds = ExternalStorage::get('TwitterTrackIds');
-//    if ($trackIds != $stream->getTrack()) {
-//        $stream->setTrack($trackIds);
-//    }
-//})->startListening();
+echo "{$twitter->consumer_key}, {$twitter->consumer_secret}, {$twitter->oauth_token}, {$twitter->oauth_secret}";
+// The OAuth credentials you received when registering your app at Twitter
+define("TWITTER_CONSUMER_KEY", $twitter->consumer_key);
+define("TWITTER_CONSUMER_SECRET", $twitter->consumer_secret);
+// The OAuth data for the twitter account
+define("OAUTH_TOKEN", $twitter->oauth_token);
+define("OAUTH_SECRET", $twitter->oauth_secret);
 
-//// Filter
-//class TwitterStreamingAPI extends OauthPhirehose{
-//
-//    public function enqueueStatus($status)
-//    {
-//        $data = json_decode($status, true);
-//        $latitude = '';
-//        $longitude = '';
-//        $text = '';
-//
-//        if (is_array($data) && isset($data['user']['screen_name'])) {
-//            $tweet = new Tweet();
-//            $text = preg_replace('/[\xF0-\xF7][\x80-\xBF][\x80-\xBF][\x80-\xBF]/', '', urldecode($data['text']));
-//            if(is_array($data['coordinates']) && isset($data['coordinates'])){
-//                $latitude  = $data['geo']['coordinates'][0];
-//                $longitude = $data['geo']['coordinates'][1];
-//            }
-//
-//            $tweet->fill([
-//                'username'          => $data['user']['screen_name'],
-//                'tweet'             => $text,
-//                'image_path'        => $data['user']['profile_image_url'],
-//                'favorite_count'    => $data['favorite_count'],
-//                'retweet_count'     => $data['retweet_count'],
-//                'latitude'          => $latitude,
-//                'longitude'         => $longitude,
-//                'tweeted_at'        => $data['created_at']
-//            ]);
-//
-//            if(!$tweet->save()){
-//                print 'error\n';
-//            }
-//        }
-//    }
-//}
-//
-//// Start streaming
-//$sc = new TwitterStreamingAPI(OAUTH_TOKEN, OAUTH_SECRET, Phirehose::METHOD_FILTER);
-//// $sc->setTrack(['テレビ', '会社', 'hello', 'the']);
-////$sc->setTrack(['災害', '防災', '地震', '豪雨', '津波', '台風', '火山', '自治体', 'ハザード']);
-//$sc->setTrack(['Disaster', 'Prevention Disaster', 'Earthquake', 'Heavy Rain', 'Flood', 'Typhoon', 'Volcanic', 'Local Government', 'Hazard']);
-//$sc->setLang('en');
-//$sc->consume();
+// Filter
+class TwitterStreamingAPI extends OauthPhirehose{
+    public function enqueueStatus($status)
+    {
+        $data = json_decode($status, true);
+        $latitude = '';
+        $longitude = '';
+        $text = '';
+
+        if (is_array($data) && isset($data['user']['screen_name'])) {
+            print $data['user']['screen_name'] . ': ' . urldecode($data['text']) . "\n";
+        }
+    }
+}
+
+// Start streaming
+$sc = new TwitterStreamingAPI(OAUTH_TOKEN, OAUTH_SECRET, Phirehose::METHOD_FILTER);
+$sc->setTrack(['テレビ', '会社', 'それ', 'the']);
+ $sc->setLang('ja');
+ $sc->consume();
