cb62c1893fe82ff8826efe293a0b295da9e25a15
tatsuya_oyanagi <17nm704r@vc.ibaraki.ac.jp>
2018-04-14 17:41:38 +0900

    Modified the function of acquiring tweet.

--- app/TwitterAPI.php
+++ app/TwitterAPI.php
@@ -35,50 +35,54 @@ class TwitterAPI extends OauthPhirehose{
     }
 
     public function enqueueStatus($status) {
-        $this->tweet = new Tweet();
         $data = json_decode($status, true);
 
         if (is_array($data) && isset($data['user']['screen_name'])) {
-            // Build a text.
-            $this->user = $data['user']['screen_name'];
             $this->text = preg_replace('/[\xF0-\xF7][\x80-\xBF][\x80-\xBF][\x80-\xBF]/', '', urldecode($data['text']));
-            $this->imagePath = $data['user']['profile_image_url'];
-            if(is_array($data['coordinates']) && isset($data['coordinates'])){
-                $this->latitude  = $data['geo']['coordinates'][0];
-                $this->longitude = $data['geo']['coordinates'][1];
-            }
 
-            if(isset($data['favorite_count'])) {
-                $this->favoriteCount = $data['favorite_count'];
-            }
+            // Remove `RT`, `reply`, `Tweet with URL`
+            if(!preg_match("/^@/", $this->text) && !preg_match("/^RT/", $this->text) && !preg_match("/[http, https]/", $this->text)) {
+                $this->tweet = new Tweet();
+                // Build a text.
+                $this->user = $data['user']['screen_name'];
+                $this->imagePath = $data['user']['profile_image_url'];
+                if (is_array($data['coordinates']) && isset($data['coordinates'])) {
+                    $this->latitude = $data['geo']['coordinates'][0];
+                    $this->longitude = $data['geo']['coordinates'][1];
+                }
 
-            if(isset($data['retweet_count'])) {
-                $this->retweetCount = $data['retweet_count'];
-            }
+                if (isset($data['favorite_count'])) {
+                    $this->favoriteCount = $data['favorite_count'];
+                }
 
-            $this->tweetedAt = $data['created_at'];
+                if (isset($data['retweet_count'])) {
+                    $this->retweetCount = $data['retweet_count'];
+                }
 
-            // Save data
-            if($this->favoriteCount > 30 || $this->retweetCount > 30) {
-                $this->tweet->fill([
-                    'username'          => $this->user,
-                    'text'             => $this->text,
-                    'image_path'        => $this->imagePath,
-                    'favorite_count'    => $this->favoriteCount,
-                    'retweet_count'     => $this->retweetCount,
-                    'latitude'          => $this->latitude,
-                    'longitude'         => $this->longitude,
-                    'tweeted_at'        => $this->tweetedAt
-                ]);
-                $this->tweet->save();
-            }
+                $this->tweetedAt = $data['created_at'];
 
-            // Send to web socket.
-            try {
-                $message = "{$this->user},{$this->text},{$this->imagePath},{$this->favoriteCount},{$this->retweetCount},{$this->latitude},{$this->longitude},{$this->tweetedAt}";
-                $this->client->send($message);
-            }catch (BadOpcodeException $e){
-                echo "An error has occurred: {$e->getMessage()}\n";;
+                // Save data
+                if ($this->favoriteCount > 30 || $this->retweetCount > 30) {
+                    $this->tweet->fill([
+                        'username' => $this->user,
+                        'text' => $this->text,
+                        'image_path' => $this->imagePath,
+                        'favorite_count' => $this->favoriteCount,
+                        'retweet_count' => $this->retweetCount,
+                        'latitude' => $this->latitude,
+                        'longitude' => $this->longitude,
+                        'tweeted_at' => $this->tweetedAt
+                    ]);
+                    $this->tweet->save();
+                }
+
+                // Send to web socket.
+                try {
+                    $message = "{$this->user},{$this->text},{$this->imagePath},{$this->favoriteCount},{$this->retweetCount},{$this->latitude},{$this->longitude},{$this->tweetedAt}";
+                    $this->client->send($message);
+                } catch (BadOpcodeException $e) {
+                    echo "An error has occurred: {$e->getMessage()}\n";;
+                }
             }
         }
     }
